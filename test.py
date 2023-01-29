a9a2f8dd2a99252ac41d09843de2ba45c41ebd1b
Ndanakari <nikita.danakari.ext@siemens.com>
2023-01-29 21:30:39 +0300

    created test script - need to be modified a lot

--- test.py
+++ test.py
@@ -1,90 +1,99 @@
-import smtplib #lib working with SMT protocol to send emails
-import os
-import mimetypes
-from tqdm import tqdm
-from email import encoders
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.image import MIMEImage
-from email.mime.audio import MIMEAudio
-from email.mime.application import MIMEApplication
-from email.mime.base import MIMEBase
-
-
-def send_email(text=None, template=None):
-    sender = "nikdanakari@gmail.com" #sender's email
-    password = os.getenv("EMAIL_PASSWORD") #getting password from env variable to keep it private
-    #password = "your password" - if you want to type it directly
-
-
-    server = smtplib.SMTP("smtp.gmail.com", 587) #587 is a default port for smtp
-    server.starttls()
-
-    #reading template to var to send it in email
-    try:
-        with open(template) as file:
-            template = file.read()
-    except IOError:
-        template = None
-
-    try:
-        server.login(sender, password)
-        msg = MIMEMultipart()
-        msg["From"] = sender
-        msg["To"] = sender
-        msg["Subject"] = ""
-
-        if text:
-            msg.attach(MIMEText(text))
-
-        if template:
-            msg.attach(MIMEText(template, "html"))
-
-        #working with different types if attachements
-        for file in tqdm(os.listdir("attachments")):
-            filename = os.path.basename(file)
-            ftype, encoding = mimetypes.guess_type(file)
-            file_type, subtype = ftype.split("/")
-            
-            if file_type == "text":
-                with open(f"attachments/{file}") as f:
-                    file = MIMEText(f.read())
-            elif file_type == "image":
-                with open(f"attachments/{file}", "rb") as f:
-                    file = MIMEImage(f.read(), subtype)
-            elif file_type == "audio":
-                with open(f"attachments/{file}", "rb") as f:
-                    file = MIMEAudio(f.read(), subtype)
-            elif file_type == "application":
-                with open(f"attachments/{file}", "rb") as f:
-                    file = MIMEApplication(f.read(), subtype)
-            else:
-                with open(f"attachments/{file}", "rb") as f:
-                    file = MIMEBase(file_type, subtype)
-                    file.set_payload(f.read())
-                    encoders.encode_base64(file)
-
-            #this works correctly too
-            # with open(f"attachments/{file}", "rb") as f:
-            #     file = MIMEBase(file_type, subtype)
-            #     file.set_payload(f.read())
-            #     encoders.encode_base64(file)
-
-            file.add_header('content-disposition', 'attachment', filename=filename)
-            msg.attach(file)
-
-        server.sendmail(sender, sender, msg.as_string())
-
-        return "The message was sent successfully!"
-    except Exception as _ex:
-        return f"{_ex}\nCheck your login or password please!"
-
-
-def main():
-    text = input("Type your text or press enter: ")
-    template = input("Type template name or press enter: ")
-    print(send_email(text=text, template=template))
-
-
-if __name__ == "__main__":
-    main()
+# importing os and pickle module in program  
+import os  
+import pickle  
+# Creating utils for Gmail APIs  
+from googleapiclient.discovery import build  
+from google_auth_oauthlib.flow import InstalledAppFlow  
+from google.auth.transport.requests import Request  
+# Importing libraries for encoding/decoding messages in base64  
+from base64 import urlsafe_b64decode, urlsafe_b64encode  
+# Importing libraries for dealing with the attachment of MIME types in Gmail  
+from email.mime.text import MIMEText  
+from email.mime.multipart import MIMEMultipart  
+from email.mime.image import MIMEImage  
+from email.mime.audio import MIMEAudio  
+from email.mime.base import MIMEBase  
+from email.mime.multipart import MIMEMultipart  
+from mimetypes import guess_type as guess_mime_type  
+  
+# Request all access from Gmail APIs and project  
+SCOPES = ['https://mail.google.com/'] # providing the scope for Gmail APIs  
+OurEmailID = 'nikdanakari@gmail.com' # giving our Gmail Id  
+  
+# using a default function to authenticate Gmail APIs  
+def authenticateGmailAPIs():  
+    creds = None  
+    # authorizing the Gmail APIs with tokens of pickles  
+    if os.path.exists("token.pickle"): # using if else statement  
+        with open("token.pickle", "rb") as token:  
+            creds = pickle.load(token)  
+    # if there are no valid credentials available in device, we will let the user sign in manually  
+    if not creds or not creds.valid:  
+        if creds and creds.expired and creds.refresh_token:  
+            creds.refresh(Request())  
+        else:  
+            flow = InstalledAppFlow.from_client_secrets_file('client_secret_301749149278-oru4c4mfakk93vkhippf4nbnvb8nplfn.apps.googleusercontent.com.json', SCOPES) # downloaded credential name  
+            creds = flow.run_local_server(port=0) # running credentials  
+        # save the credentials for the next run  
+        with open("token.pickle", "wb") as token:  
+            pickle.dump(creds, token)  
+    return build('gmail', 'v1', credentials=creds) # using Gmail to authenticate  
+  
+# Get the Gmail API service by calling the function  
+ServicesGA = authenticateGmailAPIs()  
+  
+# Using a default funnction to add attachments in Mail  
+def AddAttachment(mail, NameofFile):  
+    content_type, encoding = guess_mime_type(NameofFile)  
+    if content_type is None or encoding is not None: # defining none file type attachment  
+        content_type = 'application/octet-stream'  
+    main_type, sub_type = content_type.split('/', 1)  
+    if main_type == 'text': # defining text file type attachment  
+        fp = open(NameofFile, 'rb') # opening file  
+        msg = MIMEText(fp.read().decode(), _subtype = sub_type)  
+        fp.close()   
+    elif main_type == 'image': # defining image file type attachment  
+        fp = open(NameofFile, 'rb')  
+        msg = MIMEImage(fp.read(), _subtype = sub_type)  
+        fp.close()  
+    elif main_type == 'audio': # defining audio file type attachment  
+        fp = open(NameofFile, 'rb')  
+        msg = MIMEAudio(fp.read(), _subtype = sub_type) # reading file  
+        fp.close()  
+    else:  
+        fp = open(NameofFile, 'rb')  
+        msg = MIMEBase(main_type, sub_type)  
+        msg.set_payload(fp.read())  
+        fp.close() # closing file  
+    NameofFile = os.path.basename(NameofFile)  
+    msg.add_header('Content-Disposition', 'attachment', NameofFile = NameofFile)  
+    mail.attach(msg) # composing the mail with given attachment  
+  
+# Creating mail with a default function  
+def CreateMail(RecieverMail, SubofMail, BodyofMail, attachments=[]): # various import content of mail as function's parameter  
+    # Using if else to check if there is any attachment in mail or not  
+    if not attachments: # no attachment is given in the mail  
+        mail = MIMEText(BodyofMail) # Body of Mail  
+        mail['to'] = RecieverMail # mail ID of Reciever  
+        mail['from'] = OurEmailID # our mail ID  
+        mail['subject'] = SubofMail # Subject of Mail  
+    else: # attachment is given in the mail  
+        mail = MIMEMultipart()  
+        mail['to'] = RecieverMail  
+        mail['from'] = OurEmailID  
+        mail['subject'] = SubofMail  
+        mail.attach(MIMEText(BodyofMail))  
+        for NameofFile in attachments:  
+            AddAttachment(mail, NameofFile)  
+    return {'raw': urlsafe_b64encode(mail.as_bytes()).decode()}  
+  
+# Creating a default function to send a mail  
+def SendMail(ServicesGA, RecieverMail, SubofMail, BodyofMail, attachments=[]):  
+    return ServicesGA.users().messages().send(  
+      userId = "me",  
+      body = CreateMail(RecieverMail, SubofMail, BodyofMail, attachments)  
+    ).execute() # Body of the mail with execute() function  
+  
+# Sending an email by adding important content, i.e., Reciever's mail, Subject, Body, etc.  
+SendMail(ServicesGA, "nikdanakari@gmail.com", "Python Project i.e., This is the subject of Mail we are sendimg!",   
+            "Now, this is the body of the email we are writing and we can add only written text here!", ["test.txt", "client_secret_301749149278-oru4c4mfakk93vkhippf4nbnvb8nplfn.apps.googleusercontent.com.json"]) # calling out default SendMail() function
